18d20ab452406c79635aa9829414493ca963f299
Sven Hammann <hammanns@student.ethz.ch>
2014-06-04 16:54:01 +0200

    New version using mu and a max iteration count. Works for the toy data with these values.

--- matlab_files_sven/rpca.m
+++ matlab_files_sven/rpca.m
@@ -7,20 +7,28 @@ function [L , S] = rpca(M)
 S = zeros(size(M));
 Y = zeros(size(M));
 
-% Not sure what this t should be exactly, it is only stated that t > 0
-t = 100*eps;
+% Not sure about mu
+mu = 10;
+mu_inv = 1/mu;
+
+% This needs to be set for the algorithm to end right now
+% The convergence criterion does not seem to be met ever
+% using the toy data
+max_iter = 100;
 
 % According to the paper
 lambda = 1/sqrt(size(M,1));
 
-L = svd_threshold(M - S + Y, t);
-S = component_threshold(M - L + Y, lambda*t);
-Y = Y + (M - L - S);
+L = svd_threshold(M - S + mu_inv*Y, mu_inv);
+S = component_threshold(M - L + mu_inv*Y, lambda*mu_inv);
+Y = Y + mu*(M - L - S);
 
-while (norm(M - L - S, 'fro') / norm(M, 'fro') > eps)
-L = svd_threshold(M - S + Y, t);
-S = component_threshold(M - L + Y, lambda*t);
-Y = Y + (M - L - S);
+iter = 0;
+while (norm(M - L - S, 'fro') / norm(M, 'fro') > eps && iter < max_iter )
+L = svd_threshold(M - S + mu_inv*Y, mu_inv);
+S = component_threshold(M - L + mu_inv*Y, lambda*mu_inv);
+Y = Y + mu*(M - L - S);
+iter = iter + 1;
 end
 
 
